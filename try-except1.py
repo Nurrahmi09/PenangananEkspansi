87e7267923632855f23b102e95e5c8dbbe8033a3
Nur Rahmi <49005228+Nurrahmi09@users.noreply.github.com>
2019-10-08 00:02:03 -0700

    Update try-except1.py

--- try-except1.py
+++ try-except1.py
@@ -3,15 +3,16 @@ def main():
   print ("PROGRAM PEMBAGIAN BILANGAN")
   
   a = float (input("Masukkan a: "))
-  b = float (input (Masukkan b: "))
+  b = float (input ("Masukkan b: "))
   
   try:
     hasil = a / b 
   except ZeroDivisionError:
-    print ("\ERROR: Nilai b tidak boleh 0:)
+    print ("\ERROR: Nilai b tidak boleh 0:")
     sys.exit(1)
     
   print("\na : ", a)
-  print("b : :, hasil)
+  print("b : ", b)
+  print("a / b = ", b)
 if __name__ == "__main__":
   main()
